99c1d0d2569b893da95b0092a31912c6f87862bc
olivier-chevallier <olivier.chevallier@macbook.local>
2022-11-22 11:35:33 +0100

    💄 Design AddMoment page

--- src/views/add/AddMoment.vue
+++ src/views/add/AddMoment.vue
@@ -9,8 +9,18 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true" class="ion-padding relative">
-      <h2 class="ion-text-center">Choisis la date et l'heure à laquelle tu es disponible pour qu'on vienne chercher tes
+      <h2 class="ion-text-center">Choisis le moment auquel tu es disponible pour qu'on vienne chercher tes
         déchêts</h2>
+      <div class="moment-selection">
+        <ion-datetime presentation="date"></ion-datetime>
+        <div class="moment-selection__time">
+          <ion-text>Entre</ion-text>
+          <ion-datetime presentation="time" minuteValues="0,15,30,45"></ion-datetime>
+          <ion-text>et</ion-text>
+          <ion-datetime presentation="time" minuteValues="0,15,30,45"></ion-datetime>
+        </div>
+      </div>
+
       <fixed-bottom-container>
         <ion-button expand="block" @click="validate()">
           Choisir le lieu
@@ -31,7 +41,9 @@ import {
   IonButtons,
   IonBackButton,
   IonIcon,
-  IonPage
+  IonPage,
+  IonDatetime,
+  IonText
 } from "@ionic/vue";
 import {chevronForwardOutline} from "ionicons/icons";
 import {useRouter} from "vue-router";
@@ -44,5 +56,21 @@ const validate = () => {
 }
 </script>
 
-<style>
+<style lang="scss">
+.moment-selection {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+
+  ion-datetime {
+    border-radius: 1rem;
+  }
+
+  &__time {
+    display: flex;
+    align-items: center;
+    gap: .5rem;
+  }
+}
 </style>

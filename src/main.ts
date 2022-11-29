8c30d0e5e9a9c399da564435ea4b6fcf30d40441
Cocosmos <mipam.guillot@crea-inseec.com>
2022-11-29 19:21:05 +0100

    🚨 Remove warn of console

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,24 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import { IonicVue } from "@ionic/vue";
+import {
+  IonBadge,
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCol,
+  IonGrid,
+  IonIcon,
+  IonicVue,
+  IonItem,
+  IonLabel,
+  IonRow,
+  IonSearchbar,
+  IonText,
+} from "@ionic/vue";
 
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/vue/css/core.css";
@@ -23,11 +40,25 @@ import "@ionic/vue/css/display.css";
 /* Theme variables */
 import "./theme/variables.css";
 
-const app = createApp(App)
-  .use(IonicVue, { mode: "ios" })
-
-  .use(router);
-
+const app = createApp(App).use(IonicVue, { mode: "ios" }).use(router);
 router.isReady().then(() => {
   app.mount("#app");
 });
+
+/* Add your global components here to avoid the warn in console */
+app
+  .component("ion-icon", IonIcon)
+  .component("ion-badge", IonBadge)
+  .component("ion-grid", IonGrid)
+  .component("ion-row", IonRow)
+  .component("ion-col", IonCol)
+  .component("ion-text", IonText)
+  .component("ion-button", IonButton)
+  .component("ion-searchbar", IonSearchbar)
+  .component("ion-label", IonLabel)
+  .component("ion-item", IonItem)
+  .component("ion-card", IonCard)
+  .component("ion-card-header", IonCardHeader)
+  .component("ion-card-title", IonCardTitle)
+  .component("ion-card-subtitle", IonCardSubtitle)
+  .component("ion-card-content", IonCardContent);

c607c0dc74594fc3d12b19dafdde19f670593cf6
Cocosmos <mipam.guillot@crea-inseec.com>
2022-12-05 23:47:52 +0100

    ✅ Login page

---
+++ src/views/Auth/LoginTab.vue
@@ -0,0 +1,104 @@
+<script lang="ts" setup>
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonButton,
+  loadingController,
+} from "@ionic/vue";
+import { ref } from "vue";
+import { supabase } from "../../data/supabase";
+import PasswordShowHide from "@/components/Input/PasswordShowHide.vue";
+import TextField from "@/components/Input/TextField.vue";
+import router from "@/router";
+
+const email = ref("");
+const password = ref("");
+
+const handleLogin = async () => {
+  const loader = await loadingController.create({});
+  try {
+    await loader.present();
+    const { error, data } = await supabase.auth.signInWithPassword({
+      email: email.value,
+      password: password.value,
+    });
+    console.log(data);
+
+    if (error) throw error;
+    else router.push("/profile");
+  } catch (error: any) {
+    console.log(error);
+  } finally {
+    await loader.dismiss();
+  }
+};
+</script>
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Connexion</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content class="ion-padding">
+      <div class="ion-text-center">
+        <img src="../../assets/images/login.svg" alt="coffre fort" />
+      </div>
+
+      <form @submit.prevent="handleLogin">
+        <text-field
+          v-model="email"
+          name="email"
+          label="Email"
+          required
+          :is-error="false"
+        >
+        </text-field>
+        <ion-item fill="outline" mode="md" class="ion-margin-bottom">
+          <ion-label position="floating">Email</ion-label>
+          <ion-input
+            v-model="email"
+            name="email"
+            autocomplete="email"
+            type="email"
+            required
+          ></ion-input>
+        </ion-item>
+        <password-show-hide
+          v-model="password"
+          :error="'Mot de passe incorrect'"
+          :isError="false"
+          label="Mot de passe"
+        ></password-show-hide>
+        <ion-button
+          color="tertiary"
+          fill="clear"
+          expand="block"
+          href="/password-reset"
+          class="ion-margin-top"
+        >
+          Mot de passe oublié ?
+        </ion-button>
+
+        <ion-button type="submit" expand="block"> Connexion </ion-button>
+
+        <ion-button
+          color="tertiary"
+          fill="clear"
+          expand="block"
+          href="/signup"
+          class="ion-margin-top"
+        >
+          Pas encore de compte ? Inscrivez-vous
+        </ion-button>
+      </form>
+    </ion-content>
+  </ion-page>
+</template>

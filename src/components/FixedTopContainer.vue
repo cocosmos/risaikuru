1701b64b67c702213061f7389b05f8fd4be2bb64
cocosmos <mipam.guillot@crea-inseec.com>
2022-12-06 19:00:42 +0100

    🚧  Fixed components top to do

---
+++ src/components/FixedTopContainer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="fixed-top">
+    <slot></slot>
+  </div>
+  <div class="padding" ref="size">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted } from "vue";
+const size = ref();
+
+onMounted(() => {
+  size.value.addEventListener("rezize", () => console.log("test"));
+  if (size.value) {
+    console.log(size.value.clientHeight);
+  }
+});
+</script>
+
+<style scoped lang="scss">
+.fixed-top {
+  position: fixed;
+  width: calc(100% - var(--padding-end) - var(--padding-start));
+}
+.padding {
+  width: 0;
+  overflow: hidden;
+  padding-top: 1rem;
+  height: calc(100% - 5000px);
+}
+</style>

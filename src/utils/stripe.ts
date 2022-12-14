d17b03d6cbc4599d450e6e8ecae32c2fffa892ff
Cocosmos <mipam.guillot@crea-inseec.com>
2022-12-14 23:14:36 +0100

    ✅ Forget to add 10 stripe

--- src/utils/stripe.ts
+++ src/utils/stripe.ts
@@ -6,6 +6,7 @@ export const stripe = [
   { reward: 3, link: "https://buy.stripe.com/test_28o7u8fua2ggdb2288" },
   { reward: 5, link: "https://buy.stripe.com/test_14k01GchYaMM0og7st" },
   { reward: 8, link: "https://buy.stripe.com/test_8wMcOsa9Q8EE0og7su" },
+  { reward: 10, link: "https://buy.stripe.com/test_eVa15KchYg767QIbIN" },
   { reward: 15, link: "https://buy.stripe.com/test_fZe5m04Pw5ssc6YbIL" },
   { reward: 20, link: "https://buy.stripe.com/test_9AQ7u84Pw2gg0og8wA" },
 ];
